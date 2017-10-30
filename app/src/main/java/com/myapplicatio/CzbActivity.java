4a89756be26bae8c1a32d2196225a54b2722b9aa
pageupRuanjian <pageupruanjian2017>
2017-10-30 16:26:14 +0800

    提交信息1

---
+++ app/src/main/java/com/myapplicatio/CzbActivity.java
@@ -0,0 +1,13 @@
+package com.myapplicatio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CzbActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_czb);
+    }
+}
