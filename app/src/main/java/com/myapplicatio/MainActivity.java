d8d7d18bcd07e50b3263ecdc6efd24e1a841bd86
pageupRuanjian <pageupruanjian2017>
2017-10-30 15:41:39 +0800

    提交信息1

---
+++ app/src/main/java/com/myapplicatio/MainActivity.java
@@ -0,0 +1,15 @@
+package com.myapplicatio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String  mytest="12323424";
+        int a ,b,c;
+    }
+}
